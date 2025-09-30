6c752b7452dea70cd4b6eded4f6f89ff7eb8ed23
Sanskar jain <141724125+Sanskarjain25072003@users.noreply.github.com>
2025-10-01 05:23:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ import GenerateRouter from "./routes/Generate.js";
 const app = express()
 app.use(express.json({limit: "50mb"}))
 app.use(express.urlencoded({ extended: true}))
-app.use(cors())
+
+app.use(cors({
+  origin: 'https://pixelforge-ai.netlify.app'
+}));
 
 const PORT = process.env.PORT
 
